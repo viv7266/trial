dd0c9d40c90e77ea647fe3111e04b3b5c38cde31
viv7266 <vivek.7266@gmail.com>
2014-05-23 15:22:56 +0530

    commit1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    using NUnit.Framework;
+    [TestFixture]
+    class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, 5 + 5);
+        }
+    }
+}
