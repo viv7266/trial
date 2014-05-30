7db401a492c08945c673e1740a65f6cbd1e7fb50
viv7266 <vivek.7266@gmail.com>
2014-05-30 16:28:59 +0530

    first

---
+++ ClassLibrary1/Largest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestFunction
+{
+    public class LargestNumber
+    {
+        public static int Largest (int[] list)
+        {
+            int index, max = 0;
+            for (index = 0; index < list.Length - 1; index++) 
+            {
+                if (list[index] > max) 
+                {
+                    max = list[index];
+                }
+            }
+            return max;
+        }
+
+    }
+}
---
+++ ClassLibrary1/LargestTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LargestFunction;
+
+[TestFixture]
+    public class LargestTest
+    {
+    [Test]
+    public void LargestOf3(){
+        Assert.That(LargestNumber.Largest(new int[] {8,9,7}), Is.EqualTo(9));
+    }
+}
+
+
